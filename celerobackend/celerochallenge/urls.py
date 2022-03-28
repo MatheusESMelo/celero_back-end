38abc9bfcd3a43d6967b3505253e5da078d6e32b
matheus <Tutuuco@gmail.com>
2022-03-28 14:47:21 -0300

    :construction: Creation of the first version of the urls

---
+++ celerobackend/celerochallenge/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+app_name = "celerochallenge"
+
+urls_patterns = [
+    path(
+        "athletes-and-results-list/",
+        views.AthletesAndResultsListViewset.as_view(),
+        name="athletes_and_results_list",
+    ),
+    path(
+        "athletes-and-results-create/",
+        views.AthletesAndResultsCreateViewset.as_view(),
+        name="athletes_and_results_create",
+    ),
+    path(
+        "athletes-and-results-update/<pk_athlete>",
+        views.AthletesAndResultsUpdateViewset.as_view(),
+        name="athletes_and_results_update",
+    ),
+]
